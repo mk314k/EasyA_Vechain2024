84c0aad0d2b5f1199de1067e9120b8186a1c6706
Kartikesh Mishra <mishrakesh3.14kartik@gmail.com>
2024-06-30 06:38:17 -0700

    almost working

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,26 +2,29 @@ import {
     WalletButton,
     useWallet,
     useWalletModal,
+    useConnex
 } from '@vechain/dapp-kit-react';
 import { useEffect, useState } from 'react';
-import ProductPage from './product';
+import { ProductPage, cart, products } from './product';
+import './app.css';
 
+const vendorAddress = '0xc4beaCd5c84180b26f7E7F3a307cCf3b000BFAFB';
 
-const Navbar = () =>{
+const Navbar = ({ isConnected, setConnected }) => {
     const { account } = useWallet();
     const { open, onConnectionStatusChange } = useWalletModal();
-    const [buttonText, setButtonText] = useState('Connect Custom Button');
+    const { thor, vendor } = useConnex();
+    const [buttonText, setButtonText] = useState('');
 
     useEffect(() => {
-        const handleConnected = (address: string | null) => {
+        const handleConnected = (address) => {
             if (address) {
-                const formattedAddress = `${address.slice(
-                    0,
-                    6,
-                )}...${address.slice(-4)}`;
-                setButtonText(`Disconnect from ${formattedAddress}`);
+                // const formattedAddress = `${address.slice(0, 6)}...${address.slice(-4)}`;
+                setConnected(true);
+                setButtonText(address);
             } else {
                 setButtonText('Connect Custom Button');
+                setConnected(false);
             }
         };
 
@@ -30,27 +33,107 @@ const Navbar = () =>{
         onConnectionStatusChange(handleConnected);
     }, [account, onConnectionStatusChange]);
 
+    const handleCheckout = async () => {
+        let totalPrice = 0;
+
+        for (const id in cart) {
+            if (cart.hasOwnProperty(id)) {
+                totalPrice += cart[id] * products[id].price;
+            }
+        }
+
+        const totalWei = totalPrice *1e5; // Assuming prices are in Ether
+        console.log(totalWei);
+
+        if (isConnected && totalWei > 0) {
+            const transferABI = {"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_amount","type":"uint256"}],"name":"transfer","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"}
+            const transferMethod = thor.account('0x0000000000000000000000000000456E65726779').method(transferABI)
+            const energyClause = transferMethod.asClause(vendorAddress, `${totalWei}`)
+
+            vendor.sign('tx', [
+                {
+                    to: vendorAddress,
+                    value: `${totalWei}`,
+                    data: '0x',
+                    comment: 'Checking out'
+                },
+                {
+                    comment: 'Checking out goods',
+                    ...energyClause
+                }
+            ])
+            .signer(buttonText) // Enforce signer
+            .gas(200000) // Set maximum gas
+            .link('https://connex.vecha.in/{txid}') // User will be back to the app by the url https://connex.vecha.in/0xffff....
+            .comment('Donate 100 VET and 1000 VeThor to the author of connex')
+            .request()
+            .then(result=>{
+                console.log(result)
+            })
+        }
+    };
+
     return (
         <>
-            <div className='flex-horizonzal navbar'>
+            <nav className='flex-horizontal navbar'>
                 <WalletButton />
-                <p className='connect-status' onClick={open}>
+                <p style={{ display: 'none' }} className='connect-status' onClick={open}>
                     {buttonText}
                 </p>
+                <button onClick={handleCheckout}>Checkout</button>
+            </nav>
+        </>
+    );
+}
+
+const Header = () => {
+    return (
+        <>
+            <div className='flex-vertical'>
+                <h1>EatBett3r</h1>
             </div>
         </>
-    )
+    );
+}
+
+const LandingPage = () => {
+    return (
+        <div className='flex-vertical landing-page'>
+            <div className='landing-header'>
+                <h1>Welcome to EatBett3r</h1>
+                <p>Healthy, Sustainable, and Honest Groceries at Your Fingertips</p>
+            </div>
+            <section className='landing-content'>
+                <div className='benefits'>
+                    <h2>Why Choose EatBett3r?</h2>
+                    <ul>
+                        <li>Fresh and Organic Products</li>
+                        <li>Track Your Food's Journey</li>
+                        <li>Earn Rewards for Sustainable Choices</li>
+                        <li>Honest Reviews and Ratings</li>
+                    </ul>
+                </div>
+                <div className='cta'>
+                    <h2>Get Started</h2>
+                    <p>Connect your wallet to explore our marketplace and start shopping sustainably.</p> 
+                </div>
+            </section>
+            <footer className='landing-footer'>
+                <p>&copy; 2024 EatBett3r. All rights reserved.</p>
+            </footer>
+        </div>
+    );
 }
 
 
-
 function App() {
+    const [isConnected, setConnected] = useState(false);
 
     return (
         <>
-            <Navbar/>
-            <ProductPage/>
-            
+            <Navbar isConnected={isConnected} setConnected={setConnected} />
+            <Header/>
+            {isConnected ? <ProductPage /> : <LandingPage />}
         </>
     );
 }
--- frontend/src/product.tsx
+++ frontend/src/product.tsx
@@ -1,54 +1,491 @@
-const products = {
+import React, { useState } from 'react';
+import './card.css';
+
+export const cart = {};
+
+export const products = {
     '0x1': {
         class: 'onion',
+        vendor: 1,
         quant: 10,
-        unit: 'kg'
+        unit: 'kg',
+        travel_history: {
+            '06/05/2024': {
+                from: 'Chicago',
+                to: 'Boston',
+                medium: 'flight',
+                co2: 2000,
+            }
+        },
+        date_harvest: '06/01/2024',
+        date_expiry: '06/30/2024'
     },
     '0x2': {
         class: 'tomato',
+        vendor: 2,
         quant: 5,
-        unit: 'kg'
+        unit: 'kg',
+        travel_history: {
+            '06/02/2024': {
+                from: 'Los Angeles',
+                to: 'San Francisco',
+                medium: 'truck',
+                co2: 500,
+            }
+        },
+        date_harvest: '05/28/2024',
+        date_expiry: '06/15/2024'
     },
     '0x3': {
         class: 'potato',
+        vendor: 3,
         quant: 20,
-        unit: 'kg'
+        unit: 'kg',
+        travel_history: {
+            '06/03/2024': {
+                from: 'Denver',
+                to: 'Chicago',
+                medium: 'train',
+                co2: 800,
+            }
+        },
+        date_harvest: '05/25/2024',
+        date_expiry: '06/20/2024'
     },
     '0x4': {
         class: 'carrot',
+        vendor: 4,
         quant: 8,
-        unit: 'kg'
+        unit: 'kg',
+        travel_history: {
+            '06/04/2024': {
+                from: 'Seattle',
+                to: 'Portland',
+                medium: 'truck',
+                co2: 300,
+            }
+        },
+        date_harvest: '05/30/2024',
+        date_expiry: '06/25/2024'
     },
     '0x5': {
         class: 'broccoli',
+        vendor: 1,
         quant: 15,
-        unit: 'kg'
+        unit: 'kg',
+        travel_history: {
+            '06/01/2024': {
+                from: 'Miami',
+                to: 'Orlando',
+                medium: 'van',
+                co2: 200,
+            }
+        },
+        date_harvest: '05/27/2024',
+        date_expiry: '06/22/2024'
     },
     '0x6': {
         class: 'cabbage',
+        vendor: 2,
         quant: 12,
-        unit: 'kg'
+        unit: 'kg',
+        travel_history: {
+            '06/06/2024': {
+                from: 'Austin',
+                to: 'Dallas',
+                medium: 'truck',
+                co2: 400,
+            }
+        },
+        date_harvest: '05/31/2024',
+        date_expiry: '06/26/2024'
     },
     '0x7': {
         class: 'spinach',
+        vendor: 3,
         quant: 7,
-        unit: 'kg'
+        unit: 'kg',
+        travel_history: {
+            '06/07/2024': {
+                from: 'Boston',
+                to: 'New York',
+                medium: 'train',
+                co2: 150,
+            }
+        },
+        date_harvest: '06/02/2024',
+        date_expiry: '06/27/2024'
     },
     '0x8': {
         class: 'pepper',
+        vendor: 4,
         quant: 9,
-        unit: 'kg'
-    }
+        unit: 'kg',
+        travel_history: {
+            '06/08/2024': {
+                from: 'San Diego',
+                to: 'Las Vegas',
+                medium: 'truck',
+                co2: 600,
+            }
+        },
+        date_harvest: '06/03/2024',
+        date_expiry: '06/28/2024'
+    },
+    '0x9': {
+        class: 'onion',
+        vendor: 2,
+        quant: 14,
+        unit: 'kg',
+        travel_history: {
+            '06/01/2024': {
+                from: 'Houston',
+                to: 'Dallas',
+                medium: 'truck',
+                co2: 300,
+            }
+        },
+        date_harvest: '05/28/2024',
+        date_expiry: '06/25/2024'
+    },
+    '0xA': {
+        class: 'tomato',
+        vendor: 3,
+        quant: 18,
+        unit: 'kg',
+        travel_history: {
+            '06/02/2024': {
+                from: 'Phoenix',
+                to: 'Las Vegas',
+                medium: 'van',
+                co2: 450,
+            }
+        },
+        date_harvest: '05/30/2024',
+        date_expiry: '06/20/2024'
+    },
+    '0xB': {
+        class: 'potato',
+        vendor: 4,
+        quant: 22,
+        unit: 'kg',
+        travel_history: {
+            '06/03/2024': {
+                from: 'Dallas',
+                to: 'Houston',
+                medium: 'train',
+                co2: 700,
+            }
+        },
+        date_harvest: '05/26/2024',
+        date_expiry: '06/18/2024'
+    },
+    '0xC': {
+        class: 'carrot',
+        vendor: 1,
+        quant: 10,
+        unit: 'kg',
+        travel_history: {
+            '06/04/2024': {
+                from: 'Seattle',
+                to: 'Vancouver',
+                medium: 'truck',
+                co2: 250,
+            }
+        },
+        date_harvest: '05/29/2024',
+        date_expiry: '06/24/2024'
+    },
+    '0xD': {
+        class: 'broccoli',
+        vendor: 2,
+        quant: 12,
+        unit: 'kg',
+        travel_history: {
+            '06/01/2024': {
+                from: 'Tampa',
+                to: 'Orlando',
+                medium: 'van',
+                co2: 150,
+            }
+        },
+        date_harvest: '05/25/2024',
+        date_expiry: '06/19/2024'
+    },
+    '0xE': {
+        class: 'cabbage',
+        vendor: 3,
+        quant: 16,
+        unit: 'kg',
+        travel_history: {
+            '06/06/2024': {
+                from: 'Denver',
+                to: 'Salt Lake City',
+                medium: 'truck',
+                co2: 350,
+            }
+        },
+        date_harvest: '05/27/2024',
+        date_expiry: '06/23/2024'
+    },
+    '0xF': {
+        class: 'spinach',
+        vendor: 4,
+        quant: 9,
+        unit: 'kg',
+        travel_history: {
+            '06/07/2024': {
+                from: 'New York',
+                to: 'Philadelphia',
+                medium: 'train',
+                co2: 100,
+            }
+        },
+        date_harvest: '06/01/2024',
+        date_expiry: '06/26/2024'
+    },
+    '0x10': {
+        class: 'pepper',
+        vendor: 1,
+        quant: 11,
+        unit: 'kg',
+        travel_history: {
+            '06/08/2024': {
+                from: 'San Jose',
+                to: 'San Francisco',
+                medium: 'truck',
+                co2: 200,
+            }
+        },
+        date_harvest: '06/04/2024',
+        date_expiry: '06/27/2024'
+    },
+    '0x11': {
+        class: 'onion',
+        vendor: 3,
+        quant: 20,
+        unit: 'kg',
+        travel_history: {
+            '06/01/2024': {
+                from: 'Chicago',
+                to: 'New York',
+                medium: 'flight',
+                co2: 1800,
+            }
+        },
+        date_harvest: '05/30/2024',
+        date_expiry: '06/22/2024'
+    },
+    '0x12': {
+        class: 'tomato',
+        vendor: 4,
+        quant: 15,
+        unit: 'kg',
+        travel_history: {
+            '06/02/2024': {
+                from: 'Miami',
+                to: 'Orlando',
+                medium: 'truck',
+                co2: 250,
+            }
+        },
+        date_harvest: '05/27/2024',
+        date_expiry: '06/18/2024'
+    },
+    '0x13': {
+        class: 'potato',
+        vendor: 1,
+        quant: 25,
+        unit: 'kg',
+        travel_history: {
+            '06/03/2024': {
+                from: 'Dallas',
+                to: 'Austin',
+                medium: 'train',
+                co2: 600,
+            }
+        },
+        date_harvest: '05/24/2024',
+        date_expiry: '06/16/2024'
+    },
+    '0x14': {
+        class: 'carrot',
+        vendor: 2,
+        quant: 14,
+        unit: 'kg',
+        travel_history: {
+            '06/04/2024': {
+                from: 'San Francisco',
+                to: 'San Diego',
+                medium: 'truck',
+                co2: 300,
+            }
+        },
+        date_harvest: '05/26/2024',
+        date_expiry: '06/20/2024'
+    },
+    '0x15': {
+        class: 'broccoli',
+        vendor: 3,
+        quant: 20,
+        unit: 'kg',
+        travel_history: {
+            '06/01/2024': {
+                from: 'Seattle',
+                to: 'Portland',
+                medium: 'van',
+                co2: 200,
+            }
+        },
+        date_harvest: '05/28/2024',
+        date_expiry: '06/21/2024'
+    },
+    '0x16': {
+        class: 'cabbage',
+        vendor: 4,
+        quant: 18,
+        unit: 'kg',
+        travel_history: {
+            '06/06/2024': {
+                from: 'Atlanta',
+                to: 'Nashville',
+                medium: 'truck',
+                co2: 300,
+            }
+        },
+        date_harvest: '05/29/2024',
+        date_expiry: '06/19/2024'
+    },
+    '0x17': {
+        class: 'spinach',
+        vendor: 1,
+        quant: 11,
+        unit: 'kg',
+        travel_history: {
+            '06/07/2024': {
+                from: 'Phoenix',
+                to: 'Tucson',
+                medium: 'train',
+                co2: 120,
+            }
+        },
+        date_harvest: '06/05/2024',
+        date_expiry: '06/28/2024'
+    },
+    '0x18': {
+        class: 'pepper',
+        vendor: 2,
+        quant: 10,
+        unit: 'kg',
+        travel_history: {
+            '06/08/2024': {
+                from: 'Denver',
+                to: 'Salt Lake City',
+                medium: 'truck',
+                co2: 450,
+            }
+        },
+        date_harvest: '06/02/2024',
+        date_expiry: '06/24/2024'
+    },
+    '0x19': {
+        class: 'onion',
+        vendor: 4,
+        quant: 13,
+        unit: 'kg',
+        travel_history: {
+            '06/01/2024': {
+                from: 'New York',
+                to: 'Philadelphia',
+                medium: 'van',
+                co2: 100,
+            }
+        },
+        date_harvest: '05/31/2024',
+        date_expiry: '06/23/2024'
+    },
+    '0x1A': {
+        class: 'tomato',
+        vendor: 1,
+        quant: 12,
+        unit: 'kg',
+        travel_history: {
+            '06/02/2024': {
+                from: 'Chicago',
+                to: 'Indianapolis',
+                medium: 'truck',
+                co2: 200,
+            }
+        },
+        date_harvest: '05/25/2024',
+        date_expiry: '06/17/2024'
+    },
 };
 
-const ProductPage = () => {
+
+const productsByClass = {};
+Object.entries(products).forEach(([id, product]) => {
+    if (!productsByClass[product.class]) {
+        productsByClass[product.class] = [];
+    }
+    productsByClass[product.class].push(id);
+});
+
+export const ProductPage = () => {
+    const [showHistory, setShowHistory] = useState({});
+
+    const toggleShowHistory = (id) => {
+        setShowHistory(prevState => ({
+            ...prevState,
+            [id]: !prevState[id]
+        }));
+    };
+
+    const buyProduct = (id) => {
+        if (id in products) {
+            const product = products[id];
+            if (product.quant > 0) {
+                product.quant -= 1;
+                if (id in cart) {
+                    cart[id] += 1;
+                } else {
+                    cart[id] = 1;
+                }
+            }
+        }
+    };
+
     return (
         <>
             <div className='flex-horizontal product-container'>
                 {Object.entries(products).map(([id, product]) => (
                     <div className='product' key={id}>
-                        <p>Class: {product.class}</p>
+                        <img className='product-img' src={`./src/assets/${product.class}.jpg`} alt={product.class} />
+                        <p>{product.class}</p>
                         <p>Quantity: {product.quant} {product.unit}</p>
+                        <p>Date of Harvest: {product.date_harvest}</p>
+                        <p>Date of Expiry: {product.date_expiry}</p>
+                        <button onClick={() => buyProduct(id)}>Add to Cart</button>
+                        {product.travel_history && (
+                            <>
+                                <button onClick={() => toggleShowHistory(id)}>
+                                    {showHistory[id] ? 'Hide History' : 'Show History'}
+                                </button>
+                                {showHistory[id] && (
+                                    <div className='travel-history'>
+                                        {Object.entries(product.travel_history).map(([date, details], index) => (
+                                            <div key={index}>
+                                                <p>Date: {date}</p>
+                                                <p>From: {details.from}</p>
+                                                <p>To: {details.to}</p>
+                                                <p>Medium: {details.medium}</p>
+                                                <p>CO2 Emissions: {details.co2} kg</p>
+                                            </div>
+                                        ))}
+                                    </div>
+                                )}
+                            </>
+                        )}
                     </div>
                 ))}
             </div>
